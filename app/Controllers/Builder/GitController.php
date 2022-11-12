d93c4019414e3360ac87c881025294dc31168930
fidpro91 <ufi.alfi@gmail.com>
2022-11-12 21:31:45 +0700

    test commit

--- app/Controllers/Builder/GitController.php
+++ app/Controllers/Builder/GitController.php
@@ -17,7 +17,6 @@ class GitController extends CoreController
     {
         $post = $this->request->getPost();
         $output = shell_exec("cd /laragon/www/ci4 && git add . && git commit -m \"".$post['commit']."\" &&git ".$post['git_type']." origin ".$post['branch_name']."");
-        echo "<pre>" . $output . "</pre>";
-        $this->respond($output,200);
+        $this->respond("<pre>" . $output . "</pre>",200);
     }
 }
